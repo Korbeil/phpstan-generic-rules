c867a65c553ba8b852ce59c9a013df1fd72e47d0
Baptiste Leduc <baptiste.leduc@gmail.com>
2025-10-13 15:20:00 +0200

    Add NeverEmptyRule & refactor function rules

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+## [2.1.0] - 2025-10-13
+### Added
+- [GH#17](https://github.com/Korbeil/phpstan-generic-rules/pull/17) Add NeverEmptyRule
+- [GH#17](https://github.com/Korbeil/phpstan-generic-rules/pull/17) Refactor function rules
+
 ## [2.0.0] - 2024-11-25
 ### Added
 - [GH#14](https://github.com/Korbeil/phpstan-generic-rules/pull/14) PHPStan 2.0 support
--- src/DebugRule.php
+++ src/DebugRule.php
@@ -5,59 +5,33 @@ declare(strict_types=1);
 namespace Korbeil\GenericRules;
 
 use PhpParser\Node;
-use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
-use PHPStan\Rules\RuleErrorBuilder;
 use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * @implements Rule<Node\Expr\FuncCall>
  */
-class DebugRule implements Rule
+class DebugRule extends GenericFunctionRule implements Rule
 {
     /** @var array<string, bool> */
-    private static $functionsToAvoid = [
+    protected static $functionsToAvoid = [
         'die' => true,
         'exit' => true,
         'var_dump' => true,
     ];
 
     /**
-     * @var bool
+     * @var string
      */
-    private $enabled;
+    protected static $identifier = 'korbeil.debug';
 
     public function __construct(bool $enabled = true)
     {
-        $this->enabled = $enabled;
+        parent::__construct($enabled);
 
         if (\class_exists(VarDumper::class)) {
             self::$functionsToAvoid['dump'] = true;
             self::$functionsToAvoid['dd'] = true;
         }
-    }
-
-    public function getNodeType(): string
-    {
-        return Node\Expr\FuncCall::class;
-    }
-
-    /**
-     * @param Node\Expr\FuncCall $node
-     */
-    public function processNode(Node $node, Scope $scope): array
-    {
-        if (!$this->enabled) {
-            return [];
-        }
-
-        $errors = [];
-        if ($node->name instanceof Node\Name && array_key_exists($node->name->getLast(), self::$functionsToAvoid)) {
-            $errors[] = RuleErrorBuilder::message(sprintf('You should not use debug function (%s)', $node->name->getLast()))
-                ->identifier('korbeil.debug')
-                ->build();
-        }
-
-        return $errors;
     }
 }
